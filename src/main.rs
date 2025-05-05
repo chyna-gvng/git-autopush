647ef95fcf30ae4ec96e756c4e9efb62d848e085
chyna-gvng <angoyewally@outlook.com>
2025-05-06 01:32:33 +0300

    new: add fa

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod watcher;
+mod git_ops;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let repo_path = env::current_dir().expect("Failed to get current directory");
+    watcher::start_watching(repo_path);
 }
